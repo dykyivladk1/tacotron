45f18aad98c502e255571d3ff36ebd76d0465b9d
Vladyslav <135007865+dykyivladk1@users.noreply.github.com>
2024-06-27 17:23:18 +0200

    Add files via upload

---
+++ source/symbols.py
@@ -0,0 +1,27 @@
+# Code snippet adapted from: https://github.com/soobinseo/Tacotron-pytorch
+
+from cleaner_module import english_cleaners
+
+
+_pad = '_'
+_eos = '~'
+_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!\'(),-.:;? '
+symbols = [_pad, _eos] + list(_characters)
+
+
+symbol2id = {s: i for i, s in enumerate(symbols)}
+id2symbol = {i: s for i, s in enumerate(symbols)}
+
+def txt2seq(txt):
+    txt = english_cleaners(txt)
+    txt = ''.join([t for t in txt if t in _characters])
+    txt = txt + _eos
+    seq = [symbol2id[s] for s in txt]
+    return seq
+
+def seq2txt(seq):
+    txt = ''.join([id2symbol[i] for i in seq])
+    return txt
+
+
+
